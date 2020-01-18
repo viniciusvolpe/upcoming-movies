92dd205d9d2fceda68dedf0f1ba585a3d1afcaab
Vinicius <viniciusvolpe44@gmail.com>
2020-01-18 14:25:20 -0300

    heroku test

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,6 +3,7 @@ const moviesRouter = require("./movies_routes");
 
 module.exports = function(app) {
     const mainRouter = express.Router();
+    mainRouter.get("/", (reuqest, response) => response.status(200).send("api running"))
     moviesRouter(mainRouter);
     app.use("/api", mainRouter);
 }
