b6362126266377feaa5d36d5e9af8261878e0ca6
Vinicius <viniciusvolpe44@gmail.com>
2020-01-19 10:23:43 -0300

    Configuring prettier for react - Adding prettier dependency and .prettierrc to config this plugin

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './app.css';
+import React from "react";
+import "./app.css";
 
 function App() {
-  return <h1>test</h1>
+  return <h1>test</h1>;
 }
 
 export default App;
